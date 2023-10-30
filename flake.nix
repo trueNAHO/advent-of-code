9d51bcff247e30460406d793d872019bcad47315
NAHO <90870942+trueNAHO@users.noreply.github.com>
2023-10-30 18:49:57 +0100

    feat(flake): add initial verison

---
+++ flake.nix
@@ -0,0 +1,46 @@
+{
+  description = "NAHO's Advent of Code solutions";
+
+  inputs = {
+    flakeUtils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+    preCommitHooks = {
+      inputs = {
+        flake-utils.follows = "flakeUtils";
+        nixpkgs-stable.follows = "preCommitHooks/nixpkgs";
+        nixpkgs.follows = "nixpkgs";
+      };
+
+      url = "github:cachix/pre-commit-hooks.nix";
+    };
+  };
+
+  outputs = {
+    self,
+    flakeUtils,
+    nixpkgs,
+    preCommitHooks,
+    ...
+  }:
+    flakeUtils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        checks.preCommitHooks = preCommitHooks.lib.${system}.run {
+          hooks = {
+            alejandra.enable = true;
+            convco.enable = true;
+            typos.enable = true;
+          };
+
+          settings.alejandra.verbosity = "quiet";
+          src = ./.;
+        };
+
+        devShells.default = pkgs.mkShell {
+          inherit (self.checks.${system}.preCommitHooks) shellHook;
+        };
+      }
+    );
+}
