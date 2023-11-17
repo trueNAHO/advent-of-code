35e2bb0cf5f8fee3639bde486a6deabd99a41980
NAHO <90870942+trueNAHO@users.noreply.github.com>
2023-11-17 14:48:15 +0100

    feat(src/bin/0/1): add example

--- flake.nix
+++ flake.nix
@@ -2,11 +2,23 @@
   description = "NAHO's Advent of Code solutions";
 
   inputs = {
+    cargo2nix = {
+      inputs = {
+        flake-compat.follows = "flakeCompat";
+        flake-utils.follows = "flakeUtils";
+        nixpkgs.follows = "nixpkgs";
+      };
+
+      url = "github:cargo2nix/cargo2nix";
+    };
+
+    flakeCompat.url = "github:edolstra/flake-compat";
     flakeUtils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
     preCommitHooks = {
       inputs = {
+        flake-compat.follows = "flakeCompat";
         flake-utils.follows = "flakeUtils";
         nixpkgs-stable.follows = "preCommitHooks/nixpkgs";
         nixpkgs.follows = "nixpkgs";
@@ -18,6 +30,7 @@
 
   outputs = {
     self,
+    cargo2nix,
     flakeUtils,
     nixpkgs,
     preCommitHooks,
@@ -25,22 +38,42 @@
   }:
     flakeUtils.lib.eachDefaultSystem (
       system: let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        checks.preCommitHooks = preCommitHooks.lib.${system}.run {
-          hooks = {
-            alejandra.enable = true;
-            convco.enable = true;
-            typos.enable = true;
-            yamllint.enable = true;
-          };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [cargo2nix.overlays.default];
+        };
 
-          settings.alejandra.verbosity = "quiet";
-          src = ./.;
+        rustPkgs = pkgs.rustBuilder.makePackageSet {
+          rustVersion = "1.73.0";
+          packageFun = import ./Cargo.nix;
+        };
+      in {
+        checks = {
+          packagesTest = self.packages.${system}.test;
+
+          preCommitHooks = preCommitHooks.lib.${system}.run {
+            hooks = {
+              alejandra.enable = true;
+              clippy.enable = true;
+              convco.enable = true;
+              rustfmt.enable = true;
+              typos.enable = true;
+              yamllint.enable = true;
+            };
+
+            settings.alejandra.verbosity = "quiet";
+            src = ./.;
+          };
         };
 
         devShells.default = pkgs.mkShell {
           inherit (self.checks.${system}.preCommitHooks) shellHook;
+        };
+
+        packages = {
+          build = rustPkgs.workspace.advent_of_code {};
+          default = self.packages.${system}.build;
+          test = rustPkgs.workspace.advent_of_code {compileMode = "test";};
         };
       }
     );
